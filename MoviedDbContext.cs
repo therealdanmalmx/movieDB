8ca73f83a4521962dedc605f9aa8a2b107895c75
therealdanmalmx <danmalmx@gmail.com>
2026-02-19 14:51:12 +0100

    Remove duplicate context

--- MoviedDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using movieDB.Models;
-
-public class MoviedDbContext(DbContextOptions<MoviedDbContext> options) : DbContext(options)
-{
-    public DbSet<Movie> Movie => Set<Movie>();
-}
